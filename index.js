46cc9ef3900832c2d5ac7f2e43e27305506b3da1
Navoda Rajapakshe <nawoda1112rajapakshe@gmail.com>
2024-05-16 11:47:28 +0530

    get,post,pach,delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+app.use(express.json());
 
 require('dotenv').config();
 const Project = require('./Project');
@@ -27,7 +27,47 @@ app.get('/skill', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+//create an endpoint to add a new project
+app.post('/projects', async (req, res) => {
+    const project = new Project(req.body);
+    try {
+        const newProject = await project.save();
+        res.status(201).json(newProject);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+//create an endpoint to update a project by id
 
+app.patch('/projects/:id', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        if (project) {
+            project.set(req.body);
+            const updatedProject = await project.save();
+            res.json(updatedProject);
+
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+//Delete a project by id
+app.delete('/projects/:id', async (req, res) => {
+    try {
+        const project = await Project.findByIdAndDeleten(req.params.id);
+        if (result) {
+            res.json({ message: 'Project deleted' });
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
